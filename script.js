3c91ed1085da238440bffc75b20a426f2ac5c4a0
k-onate <kevinonatealt@gmail.com>
2024-12-12 19:50:29 -0500

    added coloring functions for cells

--- script.js
+++ script.js
@@ -104,15 +104,42 @@ function selectColor(){
 
 // Fill all uncolored cells
 function fillU(){
-    alert("Clicked Fill All Uncolored"); // Replace this line with your code.
+    let cells = document.querySelectorAll('td');
+
+    cells.forEach(cell =>
+    {
+        if(cell.style.backgroundColor == 'white' || cell.style.backgroundColor == '')
+        {
+            if(colorSelected != '' && colorSelected != 'SELECT')
+            {
+                cell.style.backgroundColor = colorSelected;
+            }
+        }
+    }
+    );
 }
 
 // Fill all cells
 function fillAll(){
-    alert("Clicked Fill All"); // Replace this line with your code.
+    const cells = document.querySelectorAll('td');
+
+    cells.forEach(cell => 
+    {
+        if(colorSelected != '' || colorSelected != 'SELECT')
+        {
+            cell.style.backgroundColor = colorSelected;
+        }
+    }
+    );
 }
 
 // Clear all cells
 function clearAll(){
-    alert("Clicked Clear All"); // Replace this line with your code.
+    const cells = document.querySelectorAll('td');
+
+    cells.forEach(cell => 
+    {
+        cell.style.backgroundColor = 'white';
+    }
+    );
 }
